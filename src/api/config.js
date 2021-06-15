3aa96cad1aff31d243391eca38b5df748e496d53
llooFlashooll <1330315641@qq.com>
2021-06-15 23:07:22 +0800

    Init commit.

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+let api_base_url = ''
+
+if (process.env.NODE_ENV === 'development') {
+    // api_base_url = 'http://localhost:3000'
+    api_base_url = "http://47.103.56.113:5000"
+} else if (process.env.NODE_ENV === 'production') {
+    api_base_url = 'https://nicemusic-api.lxhcool.cn/'
+}
+
+export default { api_base_url }
